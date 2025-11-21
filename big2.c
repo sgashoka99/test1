2bc202018f2ced14a4cd0b036a2fa97b348c1117
Cloud User <ec2-user@ip-172-31-35-188.eu-north-1.compute.internal>
2025-11-21 11:14:44 +0000

    c project

---
+++ big2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "big2.h"
+
+void biggest2()
+{
+    int x,y;
+    printf("Enter the two numbers \n");
+    scanf("%d%d",&x,&y);
+    if(x>y)
+    { 
+        printf("x is greater than y\n");
+    }else
+{
+    printf("y is greater than x\n");
+}
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "big2.h"
+#include "fact.h"
+
+int main() {
+    biggest2();
+    factorial();
+    return 0;
+}
+
